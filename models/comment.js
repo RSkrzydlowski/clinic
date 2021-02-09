9b028bb212a2415ead51cdb6239fc100b534cb9c
RSkrzydlowski <rafalskrzydlowski@gmail.com>
2021-02-09 21:37:11 +0100

    Add comment model

---
+++ models/comment.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate-v2');
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+	patient: { type: Schema.Types.ObjectId, ref: 'User' },
+	doctor: { type: Schema.Types.ObjectId, ref: 'User' },
+	comment: String,
+	rate: Number
+});
+
+CommentSchema.plugin(mongoosePaginate);
+
+module.exports = mongoose.model('Comment', CommentSchema);
