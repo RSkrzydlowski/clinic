f05816f522c99c4fe346b5f30220409a13bba608
RSkrzydlowski <rafalskrzydlowski@gmail.com>
2021-02-09 21:35:19 +0100

    Add possible to write comment to specialist

--- client/src/components/commentSection/commentSection.jsx
+++ client/src/components/commentSection/commentSection.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './commentSection.scss';
 
 const CommentSection = () => {
+  const [comment, setComment] = useState('');
+
+
   return (
-    <div>
-      <p>Oceń:</p>
-      <textarea />
+    <div className="comment_section">
+      <textarea onChange={(e) => setComment(e.target.value)} placeholder="Dodaj komentarz..." className="comment_textarea" />
+      <button onClick={() => console.log(comment)}>Dodaj</button>
     </div>
    );
 }
