9b971c5dcf1bdc94d10bbd427e6f7096ac416a15
RSkrzydlowski <rafalskrzydlowski@gmail.com>
2021-02-10 13:14:27 +0100

    Set comment element

--- client/src/components/commentElement/commentElement.jsx
+++ client/src/components/commentElement/commentElement.jsx
@@ -3,12 +3,12 @@ import './commentElement.scss';
 
 const CommentElement = (props) => {
   return (
-    <div>
+    <div className="comment_element_block">
       <div className="comment_paragraph">
         <p>{props.user}</p>
         <p>{props.date}</p>
       </div>
-      <div className="comment_element_block">
+      <div className="comment_text">
         <p>{props.comment}</p>
       </div>
     </div>
