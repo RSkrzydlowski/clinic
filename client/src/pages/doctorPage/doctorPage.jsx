4a879b0b7c2e6964f4fc826d0050b43fd8f9a21a
RSkrzydlowski <rafalskrzydlowski@gmail.com>
2021-02-11 14:10:21 +0100

    Add star bar

--- client/src/pages/doctorPage/doctorPage.jsx
+++ client/src/pages/doctorPage/doctorPage.jsx
@@ -3,6 +3,8 @@ import './doctorPage.scss';
 import { APP_URL } from '../../data/constant';
 import { AddCommentSection, CommentElement } from '../../components';
 import { AuthContext } from '../../authentication';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 const DoctorPage = ({match}) => {
   const id = match.params.id
@@ -10,6 +12,7 @@ const DoctorPage = ({match}) => {
   const [doctor, setDoctor] = useState({})
   const [counter, setCounter] = useState(0)
   const [doctorComments, setDoctorComments] = useState([]);
+  const [stars, setStars] = useState([]);
   const [visitHour, setVisitHour] = useState('');
 
 
@@ -35,6 +38,17 @@ const DoctorPage = ({match}) => {
     await fetch(url).then(async res => {
     res.json().then(res => {
       const data = res.data
+      const stars = res.data.filter(data => data.rate > 0)
+      const starsArray = [0, 0, 0, 0, 0]
+      for (let i = 0; i < stars.length; i++) {
+        starsArray[stars[i].rate - 1] += 1;
+      }
+      if(stars.length > 0) {
+        for(let i = 0; i < starsArray.length; i++) {
+          starsArray[i] = (starsArray[i] / stars.length) * 100
+        }
+      }
+      setStars(starsArray)
       setDoctorComments(data)
     });
   });
@@ -47,7 +61,12 @@ const DoctorPage = ({match}) => {
     setCounter(counter + 1)
   }
 
-  const commentSection = doctorComments.map((data) =>
+
+
+  const comments = doctorComments.filter(data => data.comment !== "")
+
+  console.table(stars)
+  const commentSection = comments.map((data) =>
     <CommentElement key={data._id} rate={data.rate} date={data.date} user={data.patient} comment={data.comment}/>
   )
 
@@ -62,11 +81,23 @@ const DoctorPage = ({match}) => {
           {doctor ? doctor.name : null}
           <p>Ocena</p>
 
-          <p>Komentarze</p>
+
 
           <p>Twoje wizyty:</p>
         </div>
+        <div>
+          <p>oceny:</p>
+          <div className="rating_statics_block">
+            <div className="element">5 <FontAwesomeIcon className="golden_star" icon={faStar} /> <div className="bar" style={{width: stars[4]}}></div></div>
+            <div className="element">4 <FontAwesomeIcon className="golden_star" icon={faStar} /> <div className="bar" style={{width: stars[3]}}></div></div>
+            <div className="element">3 <FontAwesomeIcon className="golden_star" icon={faStar} /> <div className="bar" style={{width: stars[2]}}></div></div>
+            <div className="element">2 <FontAwesomeIcon className="golden_star" icon={faStar} /> <div className="bar" style={{width: stars[1]}}></div></div>
+            <div className="element">1 <FontAwesomeIcon className="golden_star" icon={faStar} /> <div className="bar" style={{width: stars[0]}}></div></div>
+          </div>
+
+        </div>
       </div>
+      <p>Komentarze ({comments.length})</p>
      <AddCommentSection changeCounter={changeCounter} doctorId={id} patientId={currentUser.id}/>
      {doctorComments && commentSection}
     </div>
