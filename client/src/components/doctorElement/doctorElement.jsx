27b889f4cc9480dae86a88d35e71f126142b4470
RSkrzydlowski <rafalskrzydlowski@gmail.com>
2021-02-25 19:51:03 +0100

    Repair rate display

--- client/src/components/doctorElement/doctorElement.jsx
+++ client/src/components/doctorElement/doctorElement.jsx
@@ -3,11 +3,12 @@ import './doctorElement.scss';
 import LinkButton from '../linkButton';
 
 const DoctorElement = (props) => {
+  const rate = props.rate !== 'brak oceny' ? Math.round((props.rate + Number.EPSILON) * 100) / 100 : 'brak oceny'
   return (
     <div className="doctor_element_block">
       <p className="doctor_name_paragraph">{props.name}</p>
       <p></p>
-      <p>Ocena: {props.rate}</p>
+      <p>Ocena: {rate}</p>
       <LinkButton link={`/doctor/${props._id}`} text="Zobacz"/>
     </div>
    );
