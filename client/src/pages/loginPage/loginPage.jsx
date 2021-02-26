74bcf94a7bb820648a470c11a84b0490ecade7e3
RSkrzydlowski <rafalskrzydlowski@gmail.com>
2021-02-26 23:02:18 +0100

    Add form element to login page

--- client/src/pages/loginPage/loginPage.jsx
+++ client/src/pages/loginPage/loginPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import authService from "../../services/auth";
 import './loginPage.scss';
-import { Button, LinkButton } from '../../components'
+import { Button, LinkButton, FormElement } from '../../components'
 
 const LoginPage = ({history}) => {
   const [ email, setEmail ] = useState('');
@@ -10,14 +10,8 @@ const LoginPage = ({history}) => {
 
   return (
     <form className="login_form">
-      <p>
-        Email:
-        <input onChange={(e) => setEmail(e.target.value)} type="email"/>
-      </p>
-      <p>
-        Hasło:
-        <input onChange={(e) => setPassword(e.target.value)} type="password"/>
-      </p>
+      <FormElement text="Email:" onChange={(e) => setEmail(e.target.value)} type="email"/>
+      <FormElement text="Hasło:" onChange={(e) => setPassword(e.target.value)} type="password"/>
       <LinkButton text="Nie pamietasz hasła?" link="/reset-password"/>
       <p>Reset hasła</p>
       <Button
