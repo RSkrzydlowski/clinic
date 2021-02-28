e447312c269c830e5cfcef009986bb0ea5d6e3f6
RSkrzydlowski <rafalskrzydlowski@gmail.com>
2021-02-28 14:45:39 +0100

    Add send email to reset password controller

--- data/emailMessages.js
+++ data/emailMessages.js
@@ -1,9 +1,19 @@
-exports.activateAccountEmail = (email, userName, address, _id) => {
+exports.activateAccountEmail = (emailData) => {
 	const mailOptions = {
-		to: email,
+		to: emailData.email,
 		subject: 'Activate account',
-		html: `<h1>Hi ${userName}</h1>
-  <p>To complete the account registration, just click on this <a href=${address}/activate/${_id}>link</a></p>`
+		html: `<h1>Hi ${emailData.userName}</h1>
+  <p>To complete the account registration, just click on this <a href=${emailData.address}/activate/${emailData.userId}>link</a></p>`
+	};
+	return mailOptions;
+};
+
+exports.resetPasswordEmail = (emailData) => {
+	const mailOptions = {
+		to: emailData.email,
+		subject: 'Reset password',
+		html: `<h1>Hi ${emailData.userName}</h1>
+	<p>It has been reported that you forgot your password to your account, to reset your password go to the link below. If that's not you, ignore this message. <a href=${emailData.address}/reset-password/${emailData.userId}/${emailData.resetId}>link</a></p>`
 	};
 	return mailOptions;
 };
